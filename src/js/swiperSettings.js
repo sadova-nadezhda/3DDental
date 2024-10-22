ce3ee776aa10d66c4be90f60d2936fe53fbec3f4
worldofhope <n.sadova98@gmail.com>
2024-10-22 17:58:41 +0500

    update project
    created pages for post (main, inner)
    created pages 404, thanks

--- src/js/swiperSettings.js
+++ src/js/swiperSettings.js
@@ -75,4 +75,31 @@ export function initializeSwipers() {
       prevEl: ".combo-button-prev",
     },
   });
+  const postSwiper = new Swiper(".postSwiper", {
+    grabCursor: true,
+    effect: "creative",
+    creativeEffect: {
+      prev: {
+        shadow: true,
+        translate: ["-20%", 0, -1],
+      },
+      next: {
+        translate: ["100%", 0, 0],
+      },
+    },
+    navigation: false,
+    pagination: {
+      el: ".swiper-pagination",
+    },
+
+    breakpoints: {
+        1024: {
+          navigation: {
+            nextEl: ".post-hero-button-next",
+            prevEl: ".post-hero-button-prev",
+          },
+          pagination: false
+        },
+      },
+  });
 }
