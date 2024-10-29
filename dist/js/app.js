da59ad24ba1cff2a06078f0c352e204675b1dd89
worldofhope <n.sadova98@gmail.com>
2024-10-29 14:39:08 +0500

    update

--- dist/js/app.js
+++ dist/js/app.js
@@ -313,6 +313,25 @@ window.addEventListener("load", () => {
       pictureBox.style.padding = `15px`;
     }
   });
+
+  // input mask tel
+  $.fn.setCursorPosition = function (pos) {
+    if ($(this).get(0).setSelectionRange) {
+      $(this).get(0).setSelectionRange(pos, pos);
+    } else if ($(this).get(0).createTextRange) {
+      var range = $(this).get(0).createTextRange();
+      range.collapse(true);
+      range.moveEnd("character", pos);
+      range.moveStart("character", pos);
+      range.select();
+    }
+  };
+  $('input[type="tel"]')
+    .click(function () {
+      $(this).setCursorPosition(3);
+    })
+    .mask("+7 (999) 999 99 99");
+
   
   window.addEventListener("resize", () => {
     handleScroll();
